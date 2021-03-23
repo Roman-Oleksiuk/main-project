cee5b6a922b25c3231d953d762b435ff5051c42f
Roman-Oleksiuk <oleksiukroman@gmail.com>
2021-03-24 01:57:13 +0200

    add js

---
+++ resource/js/header_modal.js
@@ -0,0 +1,15 @@
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("modalBtn");
+const span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+
+
+
+
